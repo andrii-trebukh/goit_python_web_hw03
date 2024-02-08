0a7d2f819e4a75548c33d7e75be8fa70e34dde3e
andrii-trebukh <andrew.trebukh@gmail.com>
2024-02-08 14:02:35 +0200

    commit 03

--- part_2/main.py
+++ part_2/main.py
@@ -33,28 +33,30 @@ def factorize_processes(*number):
     return [out[0] for out in output]
 
 
-a, b, c, d = factorize(128, 255, 99999, 10651060)
+if __name__ == "__main__":
+    a, b, c, d = factorize(128, 255, 99999, 10651060)
 
-assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
-a, b, c, d = factorize_processes(128, 255, 99999, 10651060)
+    a, b, c, d = factorize_processes(128, 255, 99999, 10651060)
 
-assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
-speed_test_input = []
-for i in range(16):
-    out = randint(1, 99) * 10 ** 12
-    speed_test_input.append(out)
+    speed_test_input = []
+    for i in range(16):
+        out = randint(1, 99) * 10 ** 12
+        speed_test_input.append(out)
 
-print(f"Test sequnce: {speed_test_input}")
+    print(f"Test sequnce: {speed_test_input}")
 
-print(f"Sequential calculation: {how_long(factorize, *speed_test_input)} sec")
+    print(f"Sequential calculation:", end=" ")
+    print(f"{how_long(factorize, *speed_test_input)} sec")
 
-print("Parallel calculation:", end=" ")
-print(f"{how_long(factorize_processes, *speed_test_input)} sec")
+    print("Parallel calculation:", end=" ")
+    print(f"{how_long(factorize_processes, *speed_test_input)} sec")
